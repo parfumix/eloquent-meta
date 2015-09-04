f738ac62503887496a9171f3309d4c7c41c43390
Radu <radu.bordei89@gmail.com>
2015-09-04 10:22:33 +0300

    improve syncMeta function.

--- src/MetaTrait.php
+++ src/MetaTrait.php
@@ -4,6 +4,11 @@ namespace Eloquent\Meta;
 
 trait MetaTrait {
 
+    /**
+     * Get all the meta for current user .
+     *
+     * @return mixed
+     */
     public function meta() {
         return $this->morphMany(Meta::class, 'metaable');
     }
@@ -13,9 +18,42 @@ trait MetaTrait {
      *
      * @param array $meta
      * @return mixed
+     *
      */
     public function syncMeta(array $meta) {
+        $dbMeta = $this->meta()->lists('value', 'key')
+            ->toArray();
 
+        $toInsert = array_diff_key($meta, $dbMeta);
+        $toDelete = array_diff_key($dbMeta, $meta);
+        $toUpdate = array_diff_key($meta, array_merge($toInsert, $toDelete));
+
+        array_walk($toInsert, function($value, $key)  {
+            $this->meta()->saveMany([
+                new Meta([
+                    'key' => $key,
+                    'value' => $value,
+                ])
+            ]);
+        });
+
+        array_walk($toDelete, function() {
+            $this->meta()
+                ->where('key', func_get_arg(1))
+                ->delete();
+        });
+
+        array_walk($toUpdate, function($value, $key) {
+            $meta = $this->meta()
+                ->where('key', $key)
+                ->first();
+
+            if( isset($meta) )
+                $meta->fill(['value' => $value])
+                    ->save();
+        });
+
+        return $this;
     }
 
     /**
@@ -27,6 +65,10 @@ trait MetaTrait {
     public function removeMeta($key) {
         if(! is_array($key))
             $key = (array)$key;
+
+        if($key[0] == '*')
+            $this->meta()
+                ->delete();
 
         $this->meta()
             ->whereIn('key', $key)
